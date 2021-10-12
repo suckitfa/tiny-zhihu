3055dc1e18f698e0cb98655e0dc41c61f55196f5
suckitfa <254202042@qq.com>
2021-10-12 19:49:59 +0800

    feat:组件

---
+++ Desktop/tinay-zhihu/src/components/Dropdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dropdown">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle">{{
+      title
+    }}</a>
+  </div>
+  <ul class="dropdown-menu">
+    <li class="dropdown-item">
+      <a href="#">新建文章</a>
+    </li>
+    <li class="dropdown-item">
+      <a href="#">编辑资料</a>
+    </li>
+  </ul>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      requrired: true,
+      default: '标题'
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
